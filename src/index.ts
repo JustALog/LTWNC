807e43cb8cd372880a3f28622296f3e7dd36762b
JustALog <24100046@st.phenikaa-uni.edu.vn>
2026-06-22 21:23:36 +0000

    feat(api) : add sessions for api

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import "dotenv/config";
 import studentRoutes from "./routes/students";
 import enrollmentRoutes from "./routes/enrollments";
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const PORT: number = parseInt(process.env.PORT || "3000", 10);
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'default_secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: process.env.NODE_ENV === 'products' }
+}));
 app.use(express.urlencoded({ extended: true }));
 // Mount CRUD routes
 app.use("/api/student", studentRoutes);
